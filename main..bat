e89d4633049c23e8d38634ac9cbe45849c453396
fahmiamni <77756146+fahmiamni@users.noreply.github.com>
2026-07-14 22:31:18 +0800

    Edited main.bat script

--- main..bat
+++ main..bat
@@ -1,6 +1,12 @@
 @echo off
 
-:: Use CALL to run the activation 
-".venv312\Scripts\python.exe" watchdog.py
- 
+:: Activate the virtual environment
+call "D:\11 Projects\_shared\venv_312\Scripts\activate.bat"
+
+:: Change directory to where main.py is located
+cd /d "D:\GitHub\Active\AutoMeetingNotes"
+
+:: Run your Python script
+python main.py
+
 pause
